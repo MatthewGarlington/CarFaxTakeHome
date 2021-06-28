b5b9a8329376665f24106652c97f010b25831fa4
Matthew Garlington <SBMatthewG@gmail.com>
2021-06-28 12:45:42 -0400

    feat: Added Model for CarData

--- CarFaxTakeHomeProject/Model.swift
+++ CarFaxTakeHomeProject/Model.swift
@@ -6,3 +6,29 @@
 //
 
 import Foundation
+
+
+struct CarData:  Codable, Hashable  {
+    let listings: [Listings]?
+}
+
+struct Listings:  Codable, Hashable {
+    var year: Int
+    var trim: String
+    var make: String
+    var model: String
+    var currentPrice: Int
+    var mileage: Int
+    var dealer: Dealer
+    var images: Images
+}
+
+struct Images: Codable, Hashable {
+    var large: [String]
+}
+
+struct Dealer: Codable, Hashable {
+    var city: String
+    var state: String
+    var phone: String
+}

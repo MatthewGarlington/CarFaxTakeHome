442ca9de7c5731e5581c68dfa31015a3b286f0c8
Matthew Garlington <SBMatthewG@gmail.com>
2021-06-28 10:36:30 -0400

    Initial Commit

---
+++ CarFaxTakeHomeProject/CarFaxTakeHomeProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarFaxTakeHomeProjectApp.swift
+//  CarFaxTakeHomeProject
+//
+//  Created by Matthew Garlington on 6/28/21.
+//
+
+import SwiftUI
+
+@main
+struct CarFaxTakeHomeProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

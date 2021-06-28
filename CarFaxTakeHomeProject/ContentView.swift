cba3bda75c0a987a4f0f63270f39033420cccc7c
Matthew Garlington <SBMatthewG@gmail.com>
2021-06-28 10:58:05 -0400

    feat: Added UI Card in Foreach loop
    The added card present the information needed with dummy data in a protoyping phase of the UI development

--- CarFaxTakeHomeProject/ContentView.swift
+++ CarFaxTakeHomeProject/ContentView.swift
@@ -9,8 +9,55 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            ScrollView {
+                ForEach(0..<1, id: \.self)  { num in
+                    ZStack(alignment: .top) {
+                        RoundedRectangle(cornerRadius: 25.0, style: .continuous)
+                            .fill(Color.black.opacity(0.2))
+                            .frame(height: 350)
+                            .padding(.horizontal)
+                        VStack(alignment: .leading) {
+                            RoundedRectangle(cornerRadius: 25.0, style: .continuous)
+                                .fill(Color.black.opacity(0.4))
+                                .frame(height: 175)
+
+
+                            VStack(alignment: .leading) {
+                                Text("2014 Acura ILX")
+                                    .bold()
+                                HStack {
+                                    Text("28,495")
+                                        .bold()
+                                    Divider()
+                                        .background(Color.black)
+                                    Text("100k mi")
+
+                                    Divider()
+                                        .background(Color.black)
+
+                                    Text("Fredericksburg, VA")
+                                }
+                                .frame(height: 50)
+
+                                HStack {
+                                    Spacer()
+                                    VStack {
+                                        Spacer()
+                                        Text("508-965-7701")
+                                    }
+                                    Spacer()
+                                }
+                            }
+                            .padding()
+                        }
+                        .padding(.horizontal)
+
+                    }
+                }
+            }
+            .navigationTitle("CarFax")
+        }
     }
 }
 

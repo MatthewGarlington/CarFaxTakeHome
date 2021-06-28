267cf59750705fb4160150414cab207e07eabefa
Matthew Garlington <SBMatthewG@gmail.com>
2021-06-28 12:45:18 -0400

    feat: Added viewmodel for CarData
    The added viewmodel is used to make a call to the provided JSON API and parse the data using the Model for CarData

--- CarFaxTakeHomeProject/ViewModel.swift
+++ CarFaxTakeHomeProject/ViewModel.swift
@@ -5,4 +5,38 @@
 //  Created by Matthew Garlington on 6/28/21.
 //
 
-import Foundation
+
+import SwiftUI
+
+class CarDataViewModel: ObservableObject {
+    
+    @Published var carData: CarData?
+
+    init() {
+
+        let url = URL(string: "https://carfax-for-consumers.firebaseio.com/assignment.json")!
+
+        let task =  URLSession.shared.dataTask(with: url) { data, response, error in
+            guard error == nil else {
+                print ("error: \(error!)")
+                return
+            }
+            guard let data = data else {
+                print("No data")
+                return
+            }
+
+            DispatchQueue.main.async {
+
+                do {
+                    self.carData = try JSONDecoder().decode(CarData?.self, from: data)
+                    print(self.carData?.listings ?? "Error")
+                } catch let jsonError {
+
+                    print("Decoding failed for Vaccine Data", jsonError)
+                }
+            }
+        }
+        task.resume()
+    }
+}

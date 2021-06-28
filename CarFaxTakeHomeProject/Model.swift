16b7bd18ed724bbf6fa14ec4d25dc6f7aa7eef77
Matthew Garlington <SBMatthewG@gmail.com>
2021-06-28 10:45:55 -0400

    feat: Added Model and ViewModel
    The added Model and ViewModel will aid in organization of fetching the JSON data to display in the application

---
+++ CarFaxTakeHomeProject/Model.swift
@@ -0,0 +1,8 @@
+//
+//  Model.swift
+//  CarFaxTakeHomeProject
+//
+//  Created by Matthew Garlington on 6/28/21.
+//
+
+import Foundation
---
+++ CarFaxTakeHomeProject/ViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  ViewModel.swift
+//  CarFaxTakeHomeProject
+//
+//  Created by Matthew Garlington on 6/28/21.
+//
+
+import Foundation

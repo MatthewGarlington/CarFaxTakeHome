f2eda7ab7301ccff2bc85b668997bbbf8c72d461
Matthew Garlington <SBMatthewG@gmail.com>
2021-06-28 11:06:02 -0400

    refactor: Refactored CarCard into its own view
    The refactored card view takes in paremeters for each of the required data in order to input what will be retreived from the JSON fetch request

--- CarFaxTakeHomeProject/ContentView.swift
+++ CarFaxTakeHomeProject/ContentView.swift
@@ -12,48 +12,7 @@ struct ContentView: View {
         NavigationView {
             ScrollView {
                 ForEach(0..<1, id: \.self)  { num in
-                    ZStack(alignment: .top) {
-                        RoundedRectangle(cornerRadius: 25.0, style: .continuous)
-                            .fill(Color.black.opacity(0.2))
-                            .frame(height: 350)
-                            .padding(.horizontal)
-                        VStack(alignment: .leading) {
-                            RoundedRectangle(cornerRadius: 25.0, style: .continuous)
-                                .fill(Color.black.opacity(0.4))
-                                .frame(height: 175)
-
-
-                            VStack(alignment: .leading) {
-                                Text("2014 Acura ILX")
-                                    .bold()
-                                HStack {
-                                    Text("28,495")
-                                        .bold()
-                                    Divider()
-                                        .background(Color.black)
-                                    Text("100k mi")
-
-                                    Divider()
-                                        .background(Color.black)
-
-                                    Text("Fredericksburg, VA")
-                                }
-                                .frame(height: 50)
-
-                                HStack {
-                                    Spacer()
-                                    VStack {
-                                        Spacer()
-                                        Text("508-965-7701")
-                                    }
-                                    Spacer()
-                                }
-                            }
-                            .padding()
-                        }
-                        .padding(.horizontal)
-
-                    }
+                    CarCardView(year: "2014", make: "Acura", model: "ILX", price: 28495, miles: 100, location: "Fredericksburg, VA", phoneNumber: "508-965-7701")
                 }
             }
             .navigationTitle("CarFax")
@@ -64,5 +23,59 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct CarCardView: View {
+    var year: String
+    var make: String
+    var model: String
+    var price: Int
+    var miles: Int
+    var location: String
+    var phoneNumber: String
+    var body: some View {
+        ZStack(alignment: .top) {
+            RoundedRectangle(cornerRadius: 25.0, style: .continuous)
+                .fill(Color.black.opacity(0.2))
+                .frame(height: 350)
+                .padding(.horizontal)
+            VStack(alignment: .leading) {
+                RoundedRectangle(cornerRadius: 25.0, style: .continuous)
+                    .fill(Color.black.opacity(0.4))
+                    .frame(height: 175)
+
+
+                VStack(alignment: .leading) {
+                    Text("\(year) \(make) \(model)")
+                        .bold()
+                    HStack {
+                        Text("\(price)")
+                            .bold()
+                        Divider()
+                            .background(Color.black)
+                        Text("\(miles)k mi")
+
+                        Divider()
+                            .background(Color.black)
+                        
+                        Text(location)
+                    }
+                    .frame(height: 50)
+
+                    HStack {
+                        Spacer()
+                        VStack {
+                            Spacer()
+                            Text("\(phoneNumber)")
+                        }
+                        Spacer()
+                    }
+                }
+                .padding()
+            }
+            .padding(.horizontal)
+
+        }
     }
 }
